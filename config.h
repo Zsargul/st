9cc26a1faef06e70c3a9bcafe4cde806ddfc2934
Zsargul <kubakerlino@gmail.com>
2022-08-07 00:24:22 +0100

    modified:   config.h 	new file:   st 	new file:   st.o 	new file:   x.o

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Misc Termsyn";
+static char *font = "Misc Tamsyn:style=Regular:size=14:autohint=true";
 static int borderpx = 2;
 
 /*
