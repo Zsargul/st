a9527eeae502678656b1ce1c5c71a2a576cece56
Zsargul <kubakerlino@gmail.com>
2022-08-16 19:35:05 +0100

    added patches folder and changed gitignore

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.7, alphaUnfocused = 0.5;
+float alpha = 0.6, alphaUnfocused = 0.4;
 /* bg color */
 unsigned int bg = 0, bgUnfocused = 0;
 
